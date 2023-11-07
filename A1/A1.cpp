d7252d72bc308f84555dde11d26a814ec8765998
Siik-B <steven.bronner@gmail.com>
2023-11-07 23:44:18 +0100

    Add missing CMakeLists

--- A1/A1.cpp
+++ A1/A1.cpp
@@ -4,7 +4,6 @@
  - Ces fonctions afficheront leur nom et leur valeur d'argument
  - Appeler ces fonctions
  - Afficher la mémoire occupée par certains types: bool, int, float, double, char, wchar_t en utilisant sizeof
- - Bonus: Changer l'architecture ciblée en x86, que se passe-t-il? Quel est le type size_t ? Quelle est sa taille de mémoire ?
  - Bonus: Trouver les valeurs min et max pouvant être contenu dans: int, unsigned int, char, unsigned char
 */
 
